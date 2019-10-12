e46b7e0e9e534c9f72efcf25822dd26ad323c7c5
BossNESTSKyo <56186908+BossNESTSKyo@users.noreply.github.com>
2019-10-12 13:14:03 +0700

    Add files via upload
    Chỉnh sửa lại một số ý

--- main.cpp
+++ main.cpp
@@ -3,123 +3,117 @@
 
 struct Node
 {
-	Node *next;
-	int data;
+    int data;
+    Node *next;
 };
 
 typedef Node *List;
 
-Node *createNode(int x)
+Node *createNode (int x)
 {
-	Node *p = new Node;
-	if(!p)
-        exit(1);
-	p->data = x;
-	p->next = NULL;
-	return p;
+    Node *p = new Node;
+    p->data = x;
+    p->next = NULL;
+    return p;
 }
 
-void addTail(List &l, int x)
+void addTail (List &L, int x)
 {
-	Node *p = createNode(x);
-	if(!l)
-        l = p;
-	else
-	{
-		Node *r = l;
-		while (r->next)
+    Node *p = createNode(x);
+    if(!L)
+      L = p;
+    else
+    {
+        Node *r = L;
+        while (r->next)
             r = r->next;
-		r->next = p;
-		r = p;
-	}
-}
-
-void xuat(List l)
-{
-	while(l)
-    {
-		if(l->next)
-            printf ("%d -> ", l->data);
-		else
-            printf ("%d ", l->data);
-		l = l->next;
-	}
-}
-
-void nhap(List &l)
-{
-	int n, x;
-	printf ("Nhap vao so phan tu cua List : ");
-	scanf ("%d", &n);
-	for( int i = 0; i< n; i++ )
-    {
-		printf ("Nhap phan tu thu %d : ", i+1);
-		scanf ("%d", &x);
-		addTail(l,x);
-	}
-}
-
-Node *merge(List l1, List l2)
-{
-	if(!l2)
-        return l1;
-	else if(!l1)
-        return l2;
-    List l = NULL;
-	if(l1->data < l2->data)
-	{
-		l = l1;
-		l->next = merge(l1->next, l2);
+        r->next = p;
     }
-	else
+}
+
+void xuat (List L)
+{
+    Node *p;
+    for (p=L; p->next!=NULL; p = p->next)
     {
-		l = l2;
-		l->next = merge(l1, l2->next);
-	}
-	return l;
+        printf ("%d -> ", p->data);
+    }
+    printf ("%d", p->data);
 }
 
-Node *getMid(List l)
+void nhap (List &L)
 {
-	if(!l)
-        return l;
-	Node *p, *q;
-	p = q = l;
-	while(q->next && q->next->next)
-	{
-		p = p->next;
-		q = q->next->next;
-	}
-	return p;
+    int n,x;
+    printf ("Nhap so phan tu cho List: ");
+    scanf ("%d", &n);
+    for (int i=0; i<n; i++)
+    {
+        printf ("Nhap gia tri cho phan tu thu %d: ", i+1);
+        scanf ("%d", &x);
+        addTail(L,x);
+    }
 }
 
-void tach(List l, List &l1, List &l2)
+Node *merge (List L1, List L2)
 {
-	Node *mid = getMid(l);
-	l1 = l;
-	l2 = mid->next;
-	mid->next = NULL;
+    if(!L1)
+        return L2;
+    else if(!L2)
+        return L1;
+    List L = NULL;
+    if(L1->data < L2->data)
+    {
+        L = L1;
+        L->next = merge(L1->next,L2);
+    }
+    else
+    {
+        L = L2;
+        L->next = merge(L1,L2->next);
+    }
+    return L;
 }
 
-void mergeSort(List &l)
+Node *getMid (List L)
 {
-	List l1 = NULL, l2 = NULL;
-	if( !l || !l->next )
+    Node *p, *q;
+    p = q = L;
+    while (q->next && q->next->next)
+    {
+        p = p->next;
+        q = q->next->next;
+    }
+    return p;
+}
+
+void tach (List L, List &L1, List &L2)
+{
+    Node *mid = getMid(L);
+    L1 = L;
+    L2 = mid->next;
+    mid->next = NULL;
+}
+
+void mergeSort (List &L)
+{
+    List L1 = NULL, L2 = NULL;
+    if(!L || !L->next)
         return;
-	tach(l, l1, l2);
-	mergeSort(l1);
-	mergeSort(l2);
-	l = merge( l1, l2);
+    tach(L,L1,L2);
+    mergeSort(L1);
+    mergeSort(L2);
+    L = merge(L1,L2);
 }
 
 int main()
 {
-	List l;
-	l = NULL;
-	nhap(l);
-	xuat(l);
-	mergeSort(l);
-	printf ("\n");
-	xuat(l);
-	return 0;
+    List L;
+    L = NULL;
+    nhap(L);
+    printf ("List ban dau:\n");
+    xuat(L);
+    mergeSort(L);
+    printf ("\nList sau khi da sap xep:\n");
+    xuat(L);
+    return 0;
 }
