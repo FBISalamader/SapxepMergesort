6997fb10a7a59574c56b304ee129ce48af2d8337
BossNESTSKyo <56186908+BossNESTSKyo@users.noreply.github.com>
2019-10-23 23:10:05 +0700

    Add files via upload
    Sắp xếp theo tên SV

--- main.cpp
+++ main.cpp
@@ -1,67 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#define SIZE 100
+
+struct SV
+{
+    char hoTen[30];
+    char MSSV [10];
+    float t,l,h;
+    float dTB;
+};
 
 struct Node
 {
-    int data;
+    SV data;
     Node *next;
 };
 
 typedef Node *List;
 
-Node *createNode (int x)
-{
-    Node *p = new Node;
-    p->data = x;
-    p->next = NULL;
-    return p;
-}
+void nhap (List &L, SV A[]);
+void nhap1SV (SV &x);
+void addTail (List &L, SV x);
+Node *createNode (SV x);
+void xuat (List L);
+void mergeSort (List &L);
+void tach (List L, List &L1, List &L2);
+Node *getMid (List L);
+Node *merge (List L1, List L2);
 
-void addTail (List &L, int x)
+int main()
 {
-    Node *p = createNode(x);
-    if(!L)
-      L = p;
-    else
-    {
-        Node *r = L;
-        while (r->next)
-            r = r->next;
-        r->next = p;
-    }
-}
-
-void xuat (List L)
-{
+    SV A[SIZE];
+    List L;
+    L = NULL;
+    nhap(L,A);
+    xuat(L);
+    mergeSort(L);
     Node *p;
-    for (p=L; p->next!=NULL; p = p->next)
+    printf ("\nList sau khi sap xep la: ");
+    for (p=L; p->next != NULL; p = p->next)
     {
-        printf ("%d -> ", p->data);
+        printf ("%s -> ", p->data.hoTen);
     }
-    printf ("%d", p->data);
-}
-
-void nhap (List &L)
-{
-    int n,x;
-    printf ("Nhap so phan tu cho List: ");
-    scanf ("%d", &n);
-    for (int i=0; i<n; i++)
-    {
-        printf ("Nhap gia tri cho phan tu thu %d: ", i+1);
-        scanf ("%d", &x);
-        addTail(L,x);
-    }
+    printf ("%s", p->data.hoTen);
+    return 0;
 }
 
 Node *merge (List L1, List L2)
 {
-    if(!L1)
+    if (!L1)
         return L2;
-    else if(!L2)
-        return L1;
+    else
+        if (!L2)
+            return L1;
     List L = NULL;
-    if(L1->data < L2->data)
+    if (strcmp(L1->data.hoTen,L2->data.hoTen) < 0)
     {
         L = L1;
         L->next = merge(L1->next,L2);
@@ -77,7 +70,7 @@ Node *merge (List L1, List L2)
 Node *getMid (List L)
 {
     Node *p, *q;
-    p = q = L;
+    p=q=L;
     while (q->next && q->next->next)
     {
         p = p->next;
@@ -96,7 +89,7 @@ void tach (List L, List &L1, List &L2)
 
 void mergeSort (List &L)
 {
-    List L1 = NULL, L2 = NULL;
+    List L1=NULL, L2=NULL;
     if(!L || !L->next)
         return;
     tach(L,L1,L2);
@@ -105,15 +98,61 @@ void mergeSort (List &L)
     L = merge(L1,L2);
 }
 
-int main()
+void xuat (List L)
 {
-    List L;
-    L = NULL;
-    nhap(L);
-    printf ("List ban dau:\n");
-    xuat(L);
-    mergeSort(L);
-    printf ("\nList sau khi da sap xep:\n");
-    xuat(L);
-    return 0;
+    Node *p;
+    printf ("List ban dau la: ");
+    for (p=L; p->next != NULL; p = p->next)
+    {
+        printf ("%s -> ", p->data.hoTen);
+    }
+    printf ("%s", p->data.hoTen);
+}
+
+Node *createNode (SV x)
+{
+    Node *p = new Node;
+    p->data = x;
+    p->next = NULL;
+    return p;
+}
+
+void addTail (List &L, SV x)
+{
+    Node *p = createNode(x);
+    if (!L)
+        L = p;
+    else
+    {
+       Node *r = L;
+       while (r->next)
+            r = r->next;
+       r->next = p;
+    }
+}
+
+void nhap1SV (SV &x)
+{
+    fflush(stdin);
+    gets(x.hoTen);
+    gets(x.MSSV);
+    float t,l,h;
+    scanf ("%f%f%f", &t, &l, &h);
+    x.t = t;
+    x.l = l;
+    x.h = h;
+    x.dTB = (t+l+h)/3;
+}
+
+void nhap (List &L, SV A[])
+{
+    int n;
+    printf ("Nhap so phan tu cua List: ");
+    scanf ("%d", &n);
+    for (int i=0; i<n; i++)
+    {
+        printf ("Nhap thong tin cho sinh vien %d: ", i+1);
+        nhap1SV(A[i]);
+        addTail(L,A[i]);
+    }
 }
